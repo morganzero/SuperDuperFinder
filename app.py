8ac347e631bd5867debad8cce7337eeb59853499
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2024-06-19 08:24:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def run():
     return "Script started"
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=80, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=80)
